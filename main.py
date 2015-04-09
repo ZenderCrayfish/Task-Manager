6fdce6406a7a7bd9049619c9977eceec54d6afd3
Nick Iacullo <duckycrayfish@gmail.com>
2015-04-08 19:27:29 -0500

    Added Flask dependency and test app

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-//add stuff later
+from flask import Flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/')
+def index():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
